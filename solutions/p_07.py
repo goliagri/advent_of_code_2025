47090b969cd4a4774f0ff46d9401807bcd6d8b4c
goliagri <griffingolias@gmail.com>
2025-12-09 10:47:54 -0800

    P7 pt 1

---
+++ solutions/p_07.py
@@ -0,0 +1,52 @@
+#https://adventofcode.com/2025/day/7
+INPUT_FILENAME = "inputs/input_07.txt"
+
+
+def run_splitter(data):
+    
+    depth = len(data)
+    width = len(data[0])
+
+    running_beams = [0 for _ in range(width)]
+    res = 0
+
+    for i in range(depth):
+        new_beams = [0 for _ in range(width)]
+        for j in range(width):
+            if running_beams[j] == 1:
+                if data[i][j] == '^':   
+                    res += 1
+                    if j > 0:
+                        new_beams[j-1] = 1
+                    if j < width-1:
+                        new_beams[j+1] = 1
+                else:
+                    new_beams[j] = 1
+            if data[i][j] == 'S':
+                new_beams[j] = 1
+            
+        running_beams = new_beams
+
+    return res
+
+
+def get_data():
+    with open(INPUT_FILENAME) as f:
+        lines = f.readlines()
+        lines = [[a for a in s.strip()] for s in lines]
+    
+    return lines
+
+
+
+
+def main():
+    data = get_data()
+
+    p1_sol = run_splitter(data)
+
+    print('number of total beam splits {}'.format(p1_sol))
+
+
+if __name__ == "__main__":
+    main()
