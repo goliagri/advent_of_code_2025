0ae940531632cbe14203b4da65790fa2a3eb5f1c
goliagri <griffingolias@gmail.com>
2025-12-09 11:11:01 -0800

    p7 pt 2

--- solutions/p_07.py
+++ solutions/p_07.py
@@ -29,6 +29,29 @@ def run_splitter(data):
 
     return res
 
+def run_quantum_splitter(data):
+    depth = len(data)
+    width = len(data[0])
+    running_beams = [0 for _ in range(width)]
+
+    for i in range(depth):
+        new_beams = [0 for _ in range(width)]
+        for j in range(width):
+            if running_beams[j] >= 1:
+                if data[i][j] == '^':
+                    if j > 0:
+                        new_beams[j-1] += running_beams[j]
+                    if j < width - 1:
+                        new_beams[j+1] += running_beams[j]
+                else:
+                    new_beams[j] += running_beams[j]
+            if data[i][j] == 'S':
+                new_beams[j] = 1
+            
+        running_beams = new_beams
+        
+    return sum(running_beams)
+
 
 def get_data():
     with open(INPUT_FILENAME) as f:
@@ -44,8 +67,10 @@ def main():
     data = get_data()
 
     p1_sol = run_splitter(data)
+    p2_sol = run_quantum_splitter(data)
 
-    print('number of total beam splits {}'.format(p1_sol))
+    print('number of total beam splits: {}'.format(p1_sol))
+    print('number of total timelines in quantum splits: {}'.format(p2_sol))
 
 
 if __name__ == "__main__":
