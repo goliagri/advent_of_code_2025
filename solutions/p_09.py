acaa9580a7d62ea8f6cf35b8024d58ac1844000c
goliagri <griffingolias@gmail.com>
2025-12-10 13:15:28 -0800

    p10 pt1

--- solutions/p_09.py
+++ solutions/p_09.py
@@ -9,6 +9,25 @@ def largest_rectangle_area(data):
             cur_area = (abs(data[i][0]-data[j][0])+1) *((abs(data[i][1]-data[j][1]))+1)
             max_area = max(cur_area, max_area)
     return max_area
+
+def largest_inner_rectangle_area(data):
+    '''
+    find all pairs s.t. no other pairs lines cross through those lines? This assumes that the inputs actually makea cohernet geometric shape and don't say cross over themselves, I think this is true.
+
+    step 1: make list of all lines in the form of (start, end, invarient, axis) where axis = 'x' or 'y' meaning it is either a line parallel to x or y axis.
+    step 2: do logic like above, but also for each rectangle, check wether any of the lines crosses any of the lines given. 
+    '''
+    connecting_lines = []
+    for i in range(len(data)):
+        a = data[i]
+        if i<len(data-1):
+            b = data[i+1]
+        else: 
+            b = data[0]
+        
+
+
+    pass
  
 def get_data():
     with open(INPUT_FILENAME) as f:
---
+++ solutions/p_10.py
@@ -0,0 +1,74 @@
+#https://adventofcode.com/2025/day/10
+INPUT_FILENAME = "inputs/input_10.txt"
+def get_min_button_presses(data):
+    res = 0
+
+    def bfs(target, buttons):
+        
+        visited = {} #dict of frozen sets -> minimum num of steps to reach
+        frontier = [] #bfs means queue-ordered frontier in terms of frozen sets.
+
+        visited[frozenset()] = 0
+        frontier.append(frozenset())
+
+        while frontier:
+            cur_state = frontier.pop(0)
+            cur_depth = visited[cur_state]
+            if cur_state == target:
+                return cur_depth
+            next_depth = cur_depth + 1
+            for button in buttons:
+                next_state = cur_state ^ button
+                if not next_state in visited.keys():
+                    frontier.append(next_state)
+                    visited[next_state] = next_depth
+                
+        return None #failed to find any series of button presses to reach target state
+
+    for machine_spec in data:
+        #we want to use button presses to search space 
+        min_button_presses = bfs(machine_spec['lights_on'], machine_spec['buttons'])
+        if min_button_presses is None:
+            raise Exception('failed to find any solving set of button presses')
+        res += min_button_presses
+
+    return res
+
+
+def get_data():
+    res = []
+    with open(INPUT_FILENAME) as f:
+        lines = f.readlines()
+        lines = [[a.strip() for a in line.split(' ')] for line in lines]
+
+        for line in lines:
+            res.append({'lights_on': set(), 'buttons':[], 'joltage_limits':[]})
+            for a in line:
+                b = a[1:-1]
+                #is lights_on
+                if a[0] == '[':
+                    for i, indicator in enumerate(b):
+                        if indicator == '#':
+                            res[-1]['lights_on'].add(i)
+                    res[-1]['lights_on'] = frozenset(res[-1]['lights_on'])
+
+                elif a[0] == '(':
+                    b = frozenset([int(j) for j in b.split(',')])
+                    res[-1]['buttons'].append(b)
+
+                elif a[0] == '{':
+                    res[-1]['joltage_limits'] = [int(j) for j in b.split(',')]
+
+                else:
+                    raise Exception(' uknown dtype indicator {}'.format(a[0]))
+    
+    return res
+
+def main():
+    data = get_data()
+    p1_sol = get_min_button_presses(data)
+
+    print('minimum presses to turn all machines on: {}'.format(p1_sol))
+
+if __name__ == "__main__":
+    main()
